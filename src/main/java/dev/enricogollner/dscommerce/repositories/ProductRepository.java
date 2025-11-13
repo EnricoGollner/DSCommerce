77931288ae05777d942ac6479798c67d80f6354d
EnricoGollner <enricogollner.tech@gmail.com>
2025-11-13 01:51:22 -0300

    ProductRepository

---
+++ src/main/java/dev/enricogollner/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package dev.enricogollner.dscommerce.repositories;
+
+import dev.enricogollner.dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT obj FROM Product obj " +
+            "WHERE UPPER(obj.name) LIKE CONCAT('%', UPPER(:name), '%')")
+    Page<Product> searchByName(String name, Pageable pageable);
+}
