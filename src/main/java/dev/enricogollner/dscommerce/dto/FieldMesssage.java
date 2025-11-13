8b988d742174b5cbf80161630ecb8a717e39f0c3
EnricoGollner <enricogollner.tech@gmail.com>
2025-11-13 01:48:57 -0300

    DTOs

---
+++ src/main/java/dev/enricogollner/dscommerce/dto/FieldMesssage.java
@@ -0,0 +1,19 @@
+package dev.enricogollner.dscommerce.dto;
+
+public class FieldMesssage {
+    private String fieldName;
+    private String message;
+
+    public FieldMesssage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
