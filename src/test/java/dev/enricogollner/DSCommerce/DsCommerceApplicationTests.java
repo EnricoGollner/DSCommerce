13f681e4461870eb08357c1677f8d853424ad0c8
EnricoGollner <enricogollner.tech@gmail.com>
2025-11-24 02:15:43 -0300

    Init commit + entities

---
+++ src/test/java/dev/enricogollner/DSCommerce/DsCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enricogollner.DSCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
