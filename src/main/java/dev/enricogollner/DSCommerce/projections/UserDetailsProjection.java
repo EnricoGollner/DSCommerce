9fb64f10960ded42b0ac4168e75bca46dac4ee2f
EnricoGollner <enricogollner.tech@gmail.com>
2025-11-24 19:32:06 -0300

    UserRepository - Search user and role

---
+++ src/main/java/dev/enricogollner/DSCommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,8 @@
+package dev.enricogollner.DSCommerce.projections;
+
+public interface UserDetailsProjection {
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
---
+++ src/main/java/dev/enricogollner/DSCommerce/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package dev.enricogollner.DSCommerce.repositories;
+
+import dev.enricogollner.DSCommerce.entities.User;
+import dev.enricogollner.DSCommerce.projections.UserDetailsProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(nativeQuery = true, value = """
+            SELECT tu.email AS username, tu.password, tr.id AS roleId, tr.authority
+            FROM tb_user tu
+            INNER JOIN tb_user_role tur ON tu.id = tur.role_id
+            INNER JOIN tr ON tr.id = tur.role.id
+            WHERE tu.email = :email
+            """)
+    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+}
