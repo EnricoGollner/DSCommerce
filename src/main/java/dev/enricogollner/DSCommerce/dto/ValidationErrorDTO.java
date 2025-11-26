6caf5253a7b748866b66315dc6bffb7bae9bf6c2
EnricoGollner <enricogollner.tech@gmail.com>
2025-11-25 21:18:17 -0300

    Fixing logic and validation

--- src/main/java/dev/enricogollner/DSCommerce/dto/ValidationErrorDTO.java
+++ src/main/java/dev/enricogollner/DSCommerce/dto/ValidationErrorDTO.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ValidationErrorDTO extends CustomErrorDTO {
-
     private List<FieldMessageDTO> errors = new ArrayList<>();
 
     public ValidationErrorDTO(Instant timestamp, Integer status, String error, String path) {
